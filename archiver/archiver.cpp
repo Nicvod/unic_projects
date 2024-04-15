32c510f18d75a3323aa0199730eb60be5b1be927
Nicvod <107276297+Nicvod@users.noreply.github.com>
2024-04-15 15:42:55 +0300

    Update archiver.cpp

--- archiver/archiver.cpp
+++ archiver/archiver.cpp
@@ -1,20 +1,34 @@
 #include "archivator.h"
 // NOLINTBEGIN
 int main(int argc, char** argv) {
-
+    if (argc < 2) {
+        std::cerr << "too few arguments";
+        return 1;
+    }
     // I'll try change parsing
     if (argv[1][1] == 'h') {
         std::cout << "Типо архиватор(нет    :(    )";
     } else if (argv[1][1] == 'd') {
+        if (argc < 3) {
+            std::cerr << "too few arguments";
+            return 1;
+        }
         Encoder encoder(argv[2]);
         encoder.UnArchive();
-    } else {
+    } else (argv[1][1] == 'c') {
+        if (argc < 3) {
+            std::cerr << "too few arguments";
+            return 1;
+        }
         std::vector<std::string> input_files;
         for (int i = 3; i < argc; ++i) {
             input_files.push_back(argv[i]);
         }
         Coder coder(input_files, argv[2]);
         coder.Archive();
+    } else {
+        std::cerr << "unknown option: " << argv[2] << std::endl;
+        return 1;
     }
     return 0;
 }
